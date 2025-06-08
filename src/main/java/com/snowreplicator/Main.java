1e1d2601c5a49116f550fad5dd87993545e6e5bb
SnowReplicator <snowrepl@gmail.com>
2025-06-08 13:17:26 +0300

    common libs pack

---
+++ src/main/java/com/snowreplicator/Main.java
@@ -0,0 +1,11 @@
+package com.snowreplicator;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("start");
+
+        System.out.println("end");
+    }
+}
